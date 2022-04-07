5628f4abd4b7522dcf90b97ae769474e420acb0f
eemsdu <burak_eldut@hotmail.com>
2022-04-07 18:19:22 +0300

    Add project files.

---
+++ K_O_Project/Migrations/20220407142835_Initt.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace K_O_Project.Migrations
+{
+    public partial class Initt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Logins",
+                columns: new[] { "Id", "Password", "Username" },
+                values: new object[] { 1, "123", "burak" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Logins",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ K_O_Project/Models/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using K_O_Project.Models.Entities;
+using System.Reflection;
+
+namespace K_O_Project.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Entities.Exam> Exams { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<Login> Logins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=konusarak_ogren_db.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Login>().HasData(new Login
+            {
+                Id = 1,
+                Username = "burak",
+                Password = "123"
+
+            }) ;
+            
+        }
+
+
+    }
+  
+}
